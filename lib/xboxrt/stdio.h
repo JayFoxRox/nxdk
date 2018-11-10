3b7837e3f3f833ae906e0357bf503a2582eb12f1
Jannik Vogel <email@jannikvogel.de>
2018-11-10 02:53:54 +0100

    Critical fix in fread + Move some code around

--- lib/xboxrt/stdio.h
+++ lib/xboxrt/stdio.h
@@ -17,21 +17,6 @@ typedef struct {
   int handle;
 } FILE;
 
-static int fread(void* buffer, int chunk_count, int chunk_size, FILE* f) {
-  unsigned int numberOfBytesRead;
-  int r = XReadFile(f->handle, buffer, chunk_count * chunk_size, &numberOfBytesRead);
-  if (r != TRUE) {
-    // Assume 0xC0000011, thanks obama!
-    debugPrint("Read failed\n");
-    return 0;
-  }
-  if (numberOfBytesRead != chunk_count * chunk_size) {
-    debugPrint("Read too few bytes\n");
-    //while(1);
-  }
-  return numberOfBytesRead / chunk_size;
-}
-
 static int fwrite(const void* buffer, int chunk_count, int chunk_size, FILE* f) {
   unsigned int numberOfBytesWritten;
   XWriteFile(f->handle, (void*)buffer, chunk_count * chunk_size, &numberOfBytesWritten);
@@ -115,6 +100,41 @@ static int fclose(FILE* f) {
   return 0;
 }
 
+static int vprintf(const char *format, va_list ap) { //FIXME: Should be in stdarg.h
+  char buf[4096];
+  //sprintf(buf, "%s", format);
+  int r = vsprintf(buf, format, ap);
+  //debugPrint(buf);
+  FILE* f = fopen("log.txt", "ab");
+  fwrite(buf, strlen(buf), 1, f);
+  fclose(f);
+  return r;
+}
+
+static int printf(const char *format, ...) {
+  va_list args;
+  va_start (args, format);
+  int r = vprintf(format, args);
+  va_end (args);
+  return r;
+}
+
+static int fread(void* buffer, int chunk_size, int chunk_count, FILE* f) {
+  unsigned int numberOfBytesRead = 1337;
+  int r = XReadFile(f->handle, buffer, chunk_count * chunk_size, &numberOfBytesRead);
+  if (r != TRUE) {
+    // Assume 0xC0000011, thanks obama!
+    printf("Read failed %d / %d\n", numberOfBytesRead, chunk_count * chunk_size);
+    return 0;
+  }
+  if (numberOfBytesRead != chunk_count * chunk_size) {
+    printf("Read too few bytes %d / %d\n", numberOfBytesRead,  chunk_count * chunk_size);
+    //while(1);
+  }
+  return numberOfBytesRead / chunk_size;
+}
+
+
 static long ftell(FILE *stream) {
   int newFilePointer;
   int r = XSetFilePointer(stream->handle, 0, &newFilePointer,	FILE_CURRENT);
@@ -137,22 +157,4 @@ static FILE* stdout = NULL;
 static void perror(const char *s) {
 }
 
-static int vprintf(const char *format, va_list ap) { //FIXME: Should be in stdarg.h
-  char buf[4096];
-  //sprintf(buf, "%s", format);
-  int r = vsprintf(buf, format, ap);
-  //debugPrint(buf);
-  FILE* f = fopen("log.txt", "ab");
-  fwrite(buf, strlen(buf), 1, f);
-  fclose(f);
-  return r;
-}
-
-static int printf(const char *format, ...) {
-  va_list args;
-  va_start (args, format);
-  int r = vprintf(format, args);
-  va_end (args);
-  return r;
-}
 
