2e483ad9d6693a81ba55eccfab13808c0cd62aac
Jannik Vogel <email@jannikvogel.de>
2019-10-05 00:35:09 +0200

    hack to enable DBG in pbkit

--- lib/pbkit/pbkit.c
+++ lib/pbkit/pbkit.c
@@ -12,7 +12,7 @@
 // You should have received a copy of the GNU Lesser General Public
 // License along with this library; if not, see <http://www.gnu.org/licenses/>
 
-//#define DBG
+#define DBG
 //#define LOG
 
 #include <hal/video.h>
