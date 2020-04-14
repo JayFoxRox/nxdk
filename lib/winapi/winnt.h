82db36f6d1f50e6d2a4bdd6adb3d37e418da49bd
Jannik Vogel <email@jannikvogel.de>
2020-04-14 17:36:12 +0200

    Add hacky implementations for interlocked exchange functions

--- lib/winapi/winnt.h
+++ lib/winapi/winnt.h
@@ -14,4 +14,22 @@ typedef LPCWSTR LPCTSTR;
 typedef LPCSTR LPCTSTR;
 #endif
 
+static LONG64 InterlockedExchange64(LONG64 volatile *Target, LONG64 Value)
+{
+    return __atomic_exchange_n(Target, Value, __ATOMIC_ACQ_REL);
+}
+
+static PVOID InterlockedExchangePointer(PVOID volatile *Target, PVOID Value)
+{
+    return __atomic_exchange_n(Target, Value, __ATOMIC_ACQ_REL);
+}
+
+static PVOID InterlockedCompareExchangePointer(PVOID volatile *Destination, PVOID Exchange, PVOID Comperand)
+{
+  PVOID hack = *Destination; //FIXME: This isn't atomic
+  int result = __atomic_compare_exchange_n (Destination, Comperand, Exchange, 0, __ATOMIC_ACQ_REL, __ATOMIC_ACQUIRE);
+  return result ? Comperand : hack;
+}
+
+
 #endif
