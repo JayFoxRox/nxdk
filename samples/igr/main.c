7c10a30b5870e9c8f66b12587c0e79d8777f876e
Jannik Vogel <email@jannikvogel.de>
2021-04-18 23:02:14 +0200

    Forward entire SDL_GameController state to IGR

--- samples/igr/main.c
+++ samples/igr/main.c
@@ -5,6 +5,33 @@
 
 #include <SDL.h>
 
+void synchronizeIgrToSdl(IGRPadState* state, const SDL_GameController* controller) {
+  memset(state, 0x00, sizeof(IGRPadState));
+
+  state->digital_dpad_up = SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_UP);
+  state->digital_dpad_down = SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_DOWN);
+  state->digital_dpad_left = SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_LEFT);
+  state->digital_dpad_right = SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_RIGHT);
+  state->digital_start = SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_START);
+  state->digital_back = SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_BACK);
+  state->digital_left_thumb = SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_LEFTSTICK);
+  state->digital_right_thumb = SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_RIGHTSTICK);
+
+  state->analog_a = SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_A) ? 0xFF : 0x00;
+  state->analog_b = SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_B) ? 0xFF : 0x00;
+  state->analog_x = SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_X) ? 0xFF : 0x00;
+  state->analog_y = SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_Y) ? 0xFF : 0x00;
+  state->analog_black = SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_RIGHTSHOULDER) ? 0xFF : 0x00;
+  state->analog_white = SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_LEFTSHOULDER) ? 0xFF : 0x00;
+  state->analog_left_trigger = SDL_GameControllerGetAxis(controller, SDL_CONTROLLER_AXIS_TRIGGERLEFT) >> 8;
+  state->analog_right_trigger = SDL_GameControllerGetAxis(controller, SDL_CONTROLLER_AXIS_TRIGGERRIGHT) >> 8;
+
+  state->axis_left_x = SDL_GameControllerGetAxis(controller, SDL_CONTROLLER_AXIS_LEFTX);
+  state->axis_left_y = ~SDL_GameControllerGetAxis(controller, SDL_CONTROLLER_AXIS_LEFTY);
+  state->axis_right_x = SDL_GameControllerGetAxis(controller, SDL_CONTROLLER_AXIS_RIGHTX);
+  state->axis_right_y = ~SDL_GameControllerGetAxis(controller, SDL_CONTROLLER_AXIS_RIGHTY);
+}
+
 void main() {
   XVideoSetMode(640, 480, 32, REFRESH_DEFAULT);
 
@@ -21,29 +48,23 @@ void main() {
     debugClearScreen();
 
     SDL_GameControllerUpdate();
-    int is_a = SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_A);
-    int is_b = SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_B);
 
-    uint32_t esp;
-    asm("mov %%esp, %0" : "=r"(esp));
-    debugPrint("ESP 0x%08X\n", esp);
     debugPrint("Seed 0x%08X\n", seed);
-    debugPrint("Alternative: %d; Trigger: %d\n", is_a, is_b);
-    if (is_b) {
-      //FIXME: What's a common index?
-      IGRPadState state = {0};
-      state.analog_left_trigger = 0xFF;
-      state.analog_right_trigger = 0xFF;
-      if (is_a) {
-        state.digital_buttons |= 0x20; // Back
-      } else {
-        state.analog_black = 0xFF;
-      }
-      state.digital_buttons |= 0x10; // Start
-      for(int i = 0; i <= 4; i++) {
-        debugPrint("Triggering IGR for pad %d\n", i);
-        notifyIGR(i, &state, true);
-      }
+
+    IGRPadState state;
+    synchronizeIgrToSdl(&state, controller);
+
+    for(int i = 0; i < 4; i++) {
+      debugPrint("Synchronizing IGR for pad %d\n", i);
+
+      // In a real application you can also use `SDL_GameControllerGetPlayerIndex(controller)` to get the port
+      unsigned int port = i;
+
+      // In a real application you can also use `SDL_GameControllerGetAttached(controller)` to get the connection status
+      bool connected = true;
+
+      // Note that the IGR might modify the state
+      notifyIGR(port, &state, connected);
     }
   }
 
